899cbf93de69f2d78e2361bb2c35ad7b7f0495c1
David Marshall <david.marshall@springpt.com>
2025-11-25 15:02:23 -0500

    Add Relational Database using ET

---
+++ my-books/Controllers/PublishersController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Services;
+using my_books.Data.ViewModels;
+using my_books.Exceptions;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishersController : ControllerBase
+    {
+        private readonly PublishersService _publishersService;
+
+        public PublishersController(PublishersService publishersService)
+        {
+            _publishersService = publishersService;
+        }
+
+        [HttpPost("add-publisher")]
+        public IActionResult AddPublisher([FromBody] PublisherVM publisher)
+        {
+            try
+            {
+                var _publisher = _publishersService.AddPublisher(publisher);
+                return Created(nameof(AddPublisher), _publisher);
+            }
+            catch(PublisherNameException ex)
+            {
+                return BadRequest($"{ex.Message}, Publisher name: {ex.PublisherName}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("get-publisher-by-id/{id}")]
+        public IActionResult GetPublisherById(int id)
+        {
+            var _publisherData = _publishersService.GetPublisherById(id);
+            if(_publisherData == null)
+            {
+                return NotFound();
+            }
+            return Ok(_publisherData);
+        }
+
+        [HttpGet("get-publisher-books-with-authors/{id}")]
+        public IActionResult GetPublisherData(int id)
+        {
+            var _publisherData = _publishersService.GetPublisherData(id);
+            return Ok(_publisherData);
+        }
+
+
+        [HttpDelete("delete-publisher-by-id/{id}")]
+        public IActionResult DeletePublisherById(int id)
+        {
+            try
+            {
+                _publishersService.DeletePublisherById(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
